8d69c87e184903c74be292d673848df74ad3a415
IOcsicnarf <nachofrancis124@gmail.com>
2025-06-03 15:16:31 -0300

    Update Program.cs

--- 6_Francisco_M/2_PC6_4/2_PC6_4/Program.cs
+++ 6_Francisco_M/2_PC6_4/2_PC6_4/Program.cs
@@ -16,14 +16,14 @@ namespace _2_PC6_4
                 part[i] = int.Parse(Console.ReadLine());
             }
 
-            // Ordenamiento burbuja descendente
+            
             for (int i = 0; i < part.Length - 1; i++)
             {
                 for (int j = 0; j < part.Length - 1 - i; j++)
                 {
                     if (part[j] < part[j + 1])
                     {
-                        // Intercambio
+                        
                         int temp = part[j];
                         part[j] = part[j + 1];
                         part[j + 1] = temp;
@@ -31,7 +31,7 @@ namespace _2_PC6_4
                 }
             }
 
-            Console.WriteLine("\nPrecios ordenados de mayor a menor:");
+            Console.WriteLine("Precios ordenados de mayor a menor:");
             for (int i = 0; i < part.Length; i++)
             {
                 Console.WriteLine("El producto " + (i + 1) + " salió " + part[i]);
