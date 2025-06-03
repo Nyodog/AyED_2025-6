39eec6f0c5a6e29992e39ca96532c14fa3a46d2a
IOcsicnarf <nachofrancis124@gmail.com>
2025-06-03 14:32:08 -0300

    Add files via upload

---
+++ 6_Francisco_M/2_PC6_4/2_PC6_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _2_PC6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿Cuántos productos vendieron? ");
+            int cant = int.Parse(Console.ReadLine());
+            int[] part = new int[cant];
+
+            for (int i = 0; i < part.Length; i++)
+            {
+                Console.Write("¿Cuánto salió el producto " + (i + 1) + "? ");
+                part[i] = int.Parse(Console.ReadLine());
+            }
+
+            // Ordenamiento burbuja descendente
+            for (int i = 0; i < part.Length - 1; i++)
+            {
+                for (int j = 0; j < part.Length - 1 - i; j++)
+                {
+                    if (part[j] < part[j + 1])
+                    {
+                        // Intercambio
+                        int temp = part[j];
+                        part[j] = part[j + 1];
+                        part[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nPrecios ordenados de mayor a menor:");
+            for (int i = 0; i < part.Length; i++)
+            {
+                Console.WriteLine("El producto " + (i + 1) + " salió " + part[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
