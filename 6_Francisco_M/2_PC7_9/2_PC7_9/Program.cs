f3a551eb7828127af7d026b811cf855227a53d69
IOcsicnarf <nachofrancis124@gmail.com>
2025-06-17 15:05:30 -0300

    Add files via upload

---
+++ 6_Francisco_M/2_PC7_9/2_PC7_9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random(); 
+            Console.WriteLine("Ingresar el tamaño de la fila: ");
+            int fila = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el tamaño de la columna: ");
+            int colum = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[fila, colum];
+            for (int f = 0; f < fila; f++)
+            {
+                for (int c = 0; c < colum; c++)
+                {
+                    matriz[f, c] = rand.Next(1,81);
+                }
+            }
+            for (int f = 0; f < fila; f++)
+            {
+                for (int c = 0; c < colum; c++)
+                {
+                    Console.Write(matriz[f, c] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
