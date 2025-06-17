0cf8f4bfcc927d6ed355d89f1b00c1871adddf37
IOcsicnarf <nachofrancis124@gmail.com>
2025-06-17 16:59:01 -0300

    Add files via upload

--- 6_Francisco_M/2_PC7_11/2_PC7_11/Program.cs
+++ 6_Francisco_M/2_PC7_11/2_PC7_11/Program.cs
@@ -14,8 +14,6 @@ namespace _2_PC7_10
             Console.WriteLine("Ingresar el tamaño de la fila y columna: ");
             int n = int.Parse(Console.ReadLine());
             int[,] matriz1 = new int[n, n];
-            int[,] matriz2 = new int[n, n];
-            int[,] matriz3 = new int[n, n];
             for (int f = 0; f < n; f++)
             {
                 for (int c = 0; c < n; c++)
@@ -23,47 +21,23 @@ namespace _2_PC7_10
                     matriz1[f, c] = rand.Next(1, 81);
                 }
             }
-            for (int f = 0; f < n; f++)
-            {
-                for (int c = 0; c < n; c++)
-                {
-                    matriz2[f, c] = rand.Next(1, 81);
-                }
-            }
+
             Console.WriteLine("Matriz 1:");
             for (int f = 0; f < n; f++)
             {
                 for (int c = 0; c < n; c++)
-                {   
+                {
                     Console.Write(matriz1[f, c] + "\t");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("-------------------");
-            Console.WriteLine("Matriz 2:");
+            Console.WriteLine("Matriz a 90°");
             for (int f = 0; f < n; f++)
             {
                 for (int c = 0; c < n; c++)
                 {
-                    Console.Write(matriz2[f, c] + "\t");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("-------------------");
-            Console.WriteLine("Suma de matrices:");
-            for (int f = 0; f < n; f++)
-            {
-                for (int c = 0; c <n; c++)
-                {
-                    matriz3[f, c] = matriz1[f, c] + matriz2[f, c];
-                }
-            }
-
-            for (int f = 0; f < n; f++)
-            {
-                for (int c = 0; c < n; c++)
-                {
-                    Console.Write(matriz3[f, c] + "\t");
+                    Console.Write(matriz1[c, f] + "\t");
                 }
                 Console.WriteLine();
             }
